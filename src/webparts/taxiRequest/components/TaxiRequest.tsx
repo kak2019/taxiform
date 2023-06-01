2b67c8b990687559b3b84c5c6760e171176a3d5b
flynt <47708682+kak2019@users.noreply.github.com>
2023-06-01 21:42:10 +0800

    Add files via upload

--- src/webparts/taxiRequest/components/TaxiRequest.tsx
+++ src/webparts/taxiRequest/components/TaxiRequest.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import styles from "./TaxiRequest.module.scss";
-import { ITaxiRequestProps } from "./ITaxiRequestProps";
-import { TextField } from "@fluentui/react/lib/TextField";
-import { Stack, IStackProps, IStackStyles } from "@fluentui/react/lib/Stack";
+import * as React from 'react';
+import styles from './TaxiRequest.module.scss';
+import { ITaxiRequestProps } from './ITaxiRequestProps';
+import { TextField } from '@fluentui/react/lib/TextField';
+import { Stack, IStackProps, IStackStyles } from '@fluentui/react/lib/Stack';
 import {
   Dropdown,
   Toggle,
@@ -10,8 +10,16 @@ import {
   DatePicker,
   DayOfWeek,
   TimePicker,
-} from "@fluentui/react";
-import { DefaultButton, PrimaryButton } from "office-ui-fabric-react";
+} from '@fluentui/react';
+
+import { DefaultButton, PrimaryButton } from 'office-ui-fabric-react';
+import {
+  carModelOptions,
+  genderOptions,
+  payModeOptions,
+  pickupTypeOptions,
+  // statusOptions,
+} from './constants';
 
 const stackTokens = { childrenGap: 50 };
 const stackStyles: Partial<IStackStyles> = { root: { width: 650 } };
@@ -24,135 +32,128 @@ const singleColumnProps: Partial<IStackProps> = {
   styles: { root: { width: 650 } },
 };
 
-// export default class TaxiRequest extends React.Component<
-//   ITaxiRequestProps,
-//   {}
-// > 
-export default function TaxiRequest(){
-
-  
-    return (
-      <div>
-        <section>
-          <h2>[RE India] - Taxi Request - New0000044441</h2>
-          <br />
-          <h3>Requestor Information</h3>
-        </section>
-        <Stack horizontal tokens={stackTokens} styles={stackStyles}>
-          <Stack {...columnProps}>
-            <TextField label="Requestor Name" required />
-            <TextField label="Phone Number"  type="number" />
-            <Dropdown
-              placeholder="Select an option"
-              label="Gender"
-              required
-              options={[{ key: "Male", text: "Female" }]}
-            />
-            <Toggle
-              label="Alternate Approver"
-              //defaultChecked
-              onText="On"
-              offText="Off"
-              style={{ marginBottom: 4 }}
-            />
-            <Dropdown
-              placeholder="Select an option"
-              label="Paymode"
-              required
-              options={[{ key: "apple", text: "Apple" }]}
-            />
-          </Stack>
-          <Stack {...columnProps}>
-            <TextField label="Email" required readOnly />
-            <TextField label="Designation" />
-            {/* 这个得是 people picker */}
-            <Dropdown
-              placeholder="Select an option"
-              label="Manager"
-              required
-              options={[{ key: "apple", text: "Apple" }]}
-            />
-            {/* 这个得是 people picker */}
-            <Dropdown
-              placeholder="Select an option"
-              label="Approver"
-              required
-              options={[{ key: "apple", text: "Apple" }]}
-            />
-            <TextField label="Cost Center" />
-          </Stack>
-        </Stack>
-        <section>
-          <h3>Booking Details</h3>
-        </section>
-        <Stack horizontal tokens={stackTokens} styles={stackStyles}>
-          <Stack {...columnProps}>
-            <TextField label="Rental City" />
-            <Dropdown
-              placeholder="Select an option"
-              label="Car Type"
-              required
-              options={[{ key: "apple", text: "Apple" }]}
-            />
-            <TextField label="Pickup Location" required />
-            <Stack
-              styles={{ root: { width: 300 } }}
-              horizontal
-              tokens={{ childrenGap: 20 }}
-            >
-              <DatePicker
-                style={{ width: 200 }}
-                firstDayOfWeek={DayOfWeek.Sunday}
-                label="Pickerup Date"
-                strings={defaultDatePickerStrings}
-              />
-              <TimePicker label="Pickerup Time" style={{ width: 200 }} />
-            </Stack>
-          </Stack>
-          <Stack {...columnProps}>
-            <Dropdown
-              placeholder="Select an option"
-              label="Pickup Type"
-              required
-              options={[{ key: "apple", text: "Apple" }]}
-            />
-            <TextField label="Justification" />
-            <TextField label="Drop Location" />
-            <Stack
-              styles={{ root: { width: 300 } }}
-              horizontal
-              tokens={{ childrenGap: 20 }}
-            >
-              <DatePicker
-                style={{ width: 200 }}
-                firstDayOfWeek={DayOfWeek.Sunday}
-                label="Drop Date"
-                strings={defaultDatePickerStrings}
-              />
-              <TimePicker label="Drop Time" style={{ width: 200 }} />
-            </Stack>
-          </Stack>
-        </Stack>
-        <Stack horizontal tokens={stackTokens} styles={stackStyles}>
-          <Stack {...singleColumnProps}>
-            <TextField label="Additional Instruction" multiline />
-          </Stack>
-        </Stack>
-
-        <Stack
-          enableScopedSelectors
-          horizontalAlign="end"
-          style={{ paddingRight: 100, marginTop: 40, marginBottom: 20 }}
-          horizontal
-        >
-          <PrimaryButton
-            text="Submit"
-            allowDisabledFocus
-            style={{ marginRight: 24 }}
+export default function TaxiRequest() {
+  return (
+    <div>
+      <section>
+        <h2>[RE India] - Taxi Request - New0000044441</h2>
+        <br />
+        <h3>Requestor Information</h3>
+      </section>
+      <Stack horizontal tokens={stackTokens} styles={stackStyles}>
+        <Stack {...columnProps}>
+          <TextField label="Requestor Name" required />
+          <TextField label="Phone Number" type="number" />
+          <Dropdown
+            placeholder="Select an option"
+            label="Gender"
+            required
+            options={genderOptions}
           />
-          <DefaultButton text="Standard" allowDisabledFocus />
+          <Toggle
+            label="Alternate Approver"
+            //defaultChecked
+            onText="On"
+            offText="Off"
+            style={{ marginBottom: 4 }}
+          />
+          <Dropdown
+            placeholder="Select an option"
+            label="Paymode"
+            required
+            options={payModeOptions}
+          />
         </Stack>
-      </div>
-    );
-  }
+        <Stack {...columnProps}>
+          <TextField label="Email" required readOnly />
+          <TextField label="Designation" />
+          {/* 这个得是 people picker */}
+          <Dropdown
+            placeholder="Select an option"
+            label="Manager"
+            required
+            options={[{ key: 'apple', text: 'Apple' }]}
+          />
+          {/* 这个得是 people picker */}
+          <Dropdown
+            placeholder="Select an option"
+            label="Approver"
+            required
+            options={[{ key: 'apple', text: 'Apple' }]}
+          />
+          <TextField label="Cost Center" />
+        </Stack>
+      </Stack>
+      <section>
+        <h3>Booking Details</h3>
+      </section>
+      <Stack horizontal tokens={stackTokens} styles={stackStyles}>
+        <Stack {...columnProps}>
+          <TextField label="Rental City" />
+          <Dropdown
+            placeholder="Select an option"
+            label="Car Type"
+            required
+            options={carModelOptions}
+          />
+          <TextField label="Pickup Location" required />
+          <Stack
+            styles={{ root: { width: 300 } }}
+            horizontal
+            tokens={{ childrenGap: 20 }}
+          >
+            <DatePicker
+              style={{ width: 200 }}
+              firstDayOfWeek={DayOfWeek.Sunday}
+              label="Pickerup Date"
+              strings={defaultDatePickerStrings}
+            />
+            <TimePicker label="Pickerup Time" style={{ width: 200 }} />
+          </Stack>
+        </Stack>
+        <Stack {...columnProps}>
+          <Dropdown
+            placeholder="Select an option"
+            label="Pickup Type"
+            required
+            options={pickupTypeOptions}
+          />
+          <TextField label="Justification" />
+          <TextField label="Drop Location" />
+          <Stack
+            styles={{ root: { width: 300 } }}
+            horizontal
+            tokens={{ childrenGap: 20 }}
+          >
+            <DatePicker
+              style={{ width: 200 }}
+              firstDayOfWeek={DayOfWeek.Sunday}
+              label="Drop Date"
+              strings={defaultDatePickerStrings}
+            />
+            <TimePicker label="Drop Time" style={{ width: 200 }} />
+          </Stack>
+        </Stack>
+      </Stack>
+      <Stack horizontal tokens={stackTokens} styles={stackStyles}>
+        <Stack {...singleColumnProps}>
+          <TextField label="Additional Instruction" multiline />
+        </Stack>
+      </Stack>
 
+      <Stack
+        enableScopedSelectors
+        horizontalAlign="end"
+        style={{ paddingRight: 100, marginTop: 40, marginBottom: 20 }}
+        horizontal
+      >
+        <PrimaryButton
+          text="Submit"
+          allowDisabledFocus
+          style={{ marginRight: 24 }}
+        />
+        <DefaultButton text="Cancel" allowDisabledFocus />
+      </Stack>
+    </div>
+  );
+}
---
+++ src/webparts/taxiRequest/components/constants.ts
@@ -0,0 +1,29 @@
+export const genderOptions = [
+  { key: 'Male', text: 'Male' },
+  { key: 'Female', text: 'Female' },
+];
+
+export const pickupTypeOptions = [
+  { key: 'Local', text: 'Local' },
+  { key: 'Outstation', text: 'Outstation' },
+];
+
+export const carModelOptions = [
+  { key: 'Mini', text: 'Mini' },
+  { key: 'Sedan', text: 'Sedan' },
+  { key: 'innova crysta', text: 'innova crysta' },
+  { key: 'premium cars', text: 'premium cars' },
+];
+
+export const payModeOptions = [
+  { key: 'BilltoCompany', text: 'BilltoCompany' },
+  { key: 'Self Pay', text: 'Self Pay' },
+];
+
+export const statusOptions = [
+  { key: 'New', text: 'New' },
+  { key: 'Under Review', text: 'Under Review' },
+  { key: 'Approved', text: 'Approved' },
+  { key: 'Rejected', text: 'Rejected' },
+  { key: 'Booked', text: 'Booked' },
+];
